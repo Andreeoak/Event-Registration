37505fa29d9b5419d7338453dfdd397fc9a88b93
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-09 18:26:23 -0300

    Emmiting events

--- event-registration/src/App.vue
+++ event-registration/src/App.vue
@@ -4,7 +4,7 @@
     <h1 class="text-4xl fount-medium">Event Booking</h1>
     <h2 class="text-2xl fount-medium">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard v-for="i in 8" :key="i"/>
+      <EventCard v-for="i in 8" :key="i" @register="console.log('Registered!')"/>
     </section>
     <h2 class="text-2xl fount-medium">Your bookings</h2>
   </main>
--- event-registration/src/components/EventsCard.vue
+++ event-registration/src/components/EventsCard.vue
@@ -4,6 +4,13 @@
   <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{title}}</h3>
   <p class="p-4 font-medium text-xl border-b border-gray-200">{{when}}</p>
   <p class="p-4 font-medium text-xl border-b border-gray-200">{{description}}</p>
+
+  <section class="flex justify-end p-4">
+    <button @click="$emit('register')"
+    class="text-sm bg-slate-500 text-white px-4 py-2 rounded-4xl hover:bg-amber-500 transition-colors">
+      Register
+    </button>
+  </section>
 </div>
 
 </template>
@@ -14,5 +21,7 @@ defineProps({
   title: String,
   when: String,
   description: String
-})
+});
+
+defineEmits(['register']);
 </script>
