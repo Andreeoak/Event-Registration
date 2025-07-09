835b1733ca2267ced0028c6749cb08671ebedb4f
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-09 16:57:53 -0300

    establishing props

---
+++ event-registration/src/components/EventsCard.vue
@@ -0,0 +1,18 @@
+<template>
+
+<div class="border border-gray-500 bg-white rounded-r-md">
+  <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{title}}</h3>
+  <p class="p-4 font-medium text-xl border-b border-gray-200">{{when}}</p>
+  <p class="p-4 font-medium text-xl border-b border-gray-200">{{description}}</p>
+</div>
+
+</template>
+
+<script setup>
+// This is a simple component to display event details.
+defineProps({
+  title: String,
+  when: String,
+  description: String
+})
+</script>
