5c5670d1990d06c1f19d05fdf4f1e75173b807da
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-09 11:18:27 -0300

    Event cards

--- event-registration/src/App.vue
+++ event-registration/src/App.vue
@@ -3,7 +3,13 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl fount-medium">Event Booking</h1>
     <h2 class="text-2xl fount-medium">All Events</h2>
+    <section class="grid grid-cols-2 gap-8">
+      <EventCard v-for="i in 8" :key="i"/>
+    </section>
     <h2 class="text-2xl fount-medium">Your bookings</h2>
   </main>
 </template>
 
+<script setup>
+import EventCard from '@/components/EventsCard.vue';
+</script>
