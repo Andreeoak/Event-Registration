459257f23d41448c594a0e724188a3294af7221a
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-11 20:37:51 -0300

    Button variant for danger

--- event-registration/src/components/BookingItem.vue
+++ event-registration/src/components/BookingItem.vue
@@ -1,13 +1,17 @@
 
 <template>
   <SectionCard>
-    Booking Item
+    <div class="flex justify-between">
+      <div>Booking to conference</div>
+      <RoundButton variant="danger">cancel</RoundButton>
+    </div>
   </SectionCard>
 </template>
 
 
 <script setup>
 import SectionCard from '@/components/SectionCard.vue';
+import RoundButton from '@/components/RoundButton.vue';
 
 </script>
 
--- event-registration/src/components/RoundButton.vue
+++ event-registration/src/components/RoundButton.vue
@@ -1,11 +1,26 @@
 <template>
   <button @click="$emit('click')"
-    class="text-sm bg-slate-500 text-white px-4 py-2 rounded-4xl hover:bg-amber-500 transition-colors">
+    :class="[
+      'text-sm font-medium rounded-full px-3 py-1',
+      variants[variant] ??variants.default
+    ]">
     <slot></slot>
     </button>
 </template>
 
 <script setup>
+
+const variants ={
+  default:'border border-gray-200 hover:bg-gray-100',
+  danger:'bg-red-200 enabled:hover:bg-red-100'
+}
+defineProps({
+  variant:{
+    type: String,
+    default: 'default'
+  }
+});
+
 defineEmits(['click']); //prevent fallthrough of click event
 
 </script>
