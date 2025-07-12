39466cad8300122477e2c1f535c12eb860143cf7
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-12 20:27:36 -0300

    fetching data and displaying it on cards

--- event-registration/src/App.vue
+++ event-registration/src/App.vue
@@ -4,7 +4,12 @@
     <h1 class="text-4xl fount-medium">Event Booking</h1>
     <h2 class="text-2xl fount-medium">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard v-for="i in 8" :key="i" @register="console.log('Registered!')"/>
+      <EventCard v-for="event in events"
+      :key="event.id"
+      :title="event.title"
+      :when="event.date"
+      :description="event.description"
+      @register="console.log('Registered!')"/>
     </section>
     <h2 class="text-2xl fount-medium">Your bookings</h2>
     <section class="grid grid-cols-1 gap-8">
@@ -14,6 +19,15 @@
 </template>
 
 <script setup>
+import { ref, onMounted} from 'vue';
 import EventCard from '@/components/EventsCard.vue';
 import BookingItem from './components/BookingItem.vue';
+
+const events = ref([]);
+const fetchEvents= async ()=>{
+    const response = await fetch("http://localhost:3001/events");
+    events.value = await response.json();
+}
+
+onMounted(()=>fetchEvents());
 </script>
