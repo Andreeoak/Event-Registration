d3d5899ccc3f4fa6067a0aa57d898bc847fe115c
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-14 20:27:50 -0300

    Responsive design

--- event-registration/src/components/EventList.vue
+++ event-registration/src/components/EventList.vue
@@ -28,25 +28,27 @@
       </SectionCard>
     </template>
     <template v-else>
-      <template v-if="!loading">
-        <template v-if="events.length">
-          <EventCard
-            v-for="event in events"
-            :key="event.id"
-            :title="event.title"
-            :when="event.date"
-            :description="event.description"
-            @register="$emit('register', event)"
-          />
+      <section class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <template v-if="!loading">
+          <template v-if="events.length">
+            <EventCard
+              v-for="event in events"
+              :key="event.id"
+              :title="event.title"
+              :when="event.date"
+              :description="event.description"
+              @register="$emit('register', event)"
+            />
+          </template>
+          <template v-else>
+            <div class="col-span-2 text-center text-gray-500">No events...</div>
+          </template>
         </template>
-        <template v-else>
-          <div class="col-span-2 text-center text-gray-500">No events...</div>
-        </template>
-      </template>
 
-      <template v-else>
-        <LoadingEventCard v-for="i in 4" :key="i" />
-      </template>
+        <template v-else>
+          <LoadingEventCard v-for="i in 4" :key="i" />
+        </template>
+      </section>
     </template>
   </section>
 </template>
